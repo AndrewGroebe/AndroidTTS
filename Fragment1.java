6a74214f3bc82372aece29763f709c4d09e47ac9
Andy Groebe <40124525+AndyGroebe@users.noreply.github.com>
2019-02-13 17:29:02 -0600

    Update Fragment1.java

--- Fragment1.java
+++ Fragment1.java
@@ -1,10 +1,10 @@
-package com.example.groebe.myapplication;
+package com.example.groebe.myapplication
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 /**
  * The first fragment, or page, of the application's GUI. In the context of the application's functionality, this
@@ -12,13 +12,12 @@ import android.view.ViewGroup;
  *
  * @author Andrew Groebe.
  */
-public final class Fragment1 extends Fragment {
+class Fragment1 : Fragment() {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-        if (container == null) {
-            return null;
-        }
-        return inflater.inflate(R.layout.fragment1_layout, container, false);
+    fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstance: Bundle): View? {
+        return if (container == null) {
+            null
+        } else inflater.inflate(R.layout.fragment1_layout, container, false)
     }
-    
+
 }
